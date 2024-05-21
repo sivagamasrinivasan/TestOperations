b2a27d1e653e4a0cc3af1820d4ca039f357ddf0a
sivagamasrinivasan <784108@pdsb.net>
2024-05-21 16:39:40 -0400

    version 2 of example

--- src/operationsgit/OperationsGit.java
+++ src/operationsgit/OperationsGit.java
@@ -13,9 +13,11 @@ public class OperationsGit {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         // TODO code application logic here
         //testing
+     
     }
     
 }
